5f75cd2bbce2b04d88121551e1fbefe1daf00c3c
Gabriel Muras Fuentes Jazra <gabriel.jazra@gmail.com>
2024-07-08 17:43:11 -0300

    add terraform for using the modules on root

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
 module "vpc" {
-    source = "./terraform-modules/network/vpc"
-    vpc_cidr_block = var.vpc_cidr_block[terraform.workspace]
-    subnets = var.subnets[terraform.workspace]
-    gateway = var.gateway[terraform.workspace]
-    routes = var.routes[terraform.workspace]
+  source         = "./terraform-modules/network/vpc"
+  vpc_cidr_block = var.vpc_cidr_block[terraform.workspace]
+  subnets        = var.subnets[terraform.workspace]
+  gateway_name   = var.gateway_name[terraform.workspace]
+  routes         = var.routes[terraform.workspace]
+
+}
+
+module "eks" {
+  source                   = "./terraform-modules/eks"
+  eks_cluster_name         = var.eks_cluster_name[terraform.workspace]
+  eks_cluster_role_name    = var.eks_cluster_role_name[terraform.workspace]
+  eks_node_group_role_name = var.eks_node_group_role_name[terraform.workspace]
+  eks_node_group           = var.eks_node_group[terraform.workspace]
+  subnet_ids               = module.vpc.subnet_ids
+  eks_addons               = var.eks_addons[terraform.workspace]
 
 }
--- provider.tf
+++ provider.tf
@@ -11,5 +11,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "= 5.57.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "= 2.31.0"
+    }
   }
 }
