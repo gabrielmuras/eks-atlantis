3038212a9dc2b30479ef91729e02f7eeb1cbff74
Gabriel Muras Fuentes Jazra <gabriel.jazra@gmail.com>
2024-07-09 20:37:43 -0300

    remove workspace vars to simplify the apply

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 module "vpc" {
   source         = "./terraform-modules/network/vpc"
-  vpc_cidr_block = var.vpc_cidr_block[terraform.workspace]
-  subnets        = var.subnets[terraform.workspace]
-  gateway_name   = var.gateway_name[terraform.workspace]
-  routes         = var.routes[terraform.workspace]
+  vpc_cidr_block = var.vpc_cidr_block
+  subnets        = var.subnets
+  gateway_name   = var.gateway_name
+  routes         = var.routes
 
 }
 
 module "eks" {
   source                   = "./terraform-modules/eks"
-  eks_cluster_name         = var.eks_cluster_name[terraform.workspace]
-  eks_cluster_role_name    = var.eks_cluster_role_name[terraform.workspace]
-  eks_node_group_role_name = var.eks_node_group_role_name[terraform.workspace]
-  eks_node_group           = var.eks_node_group[terraform.workspace]
+  eks_cluster_name         = var.eks_cluster_name
+  eks_cluster_role_name    = var.eks_cluster_role_name
+  eks_node_group_role_name = var.eks_node_group_role_name
+  eks_node_group           = var.eks_node_group
   subnet_ids               = module.vpc.subnet_ids
-  eks_addons               = var.eks_addons[terraform.workspace]
+  eks_addons               = var.eks_addons
 
   depends_on = [module.vpc]
 
@@ -22,7 +22,7 @@ module "eks" {
 
 module "load-balancer-controller" {
   source                           = "./terraform-modules/network/load-balancer-controller"
-  cluster_name                     = var.eks_cluster_name[terraform.workspace]
+  cluster_name                     = var.eks_cluster_name
   cluster_identity_oidc_issuer     = data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer
   cluster_identity_oidc_issuer_arn = data.aws_iam_openid_connect_provider.oidc_provider.arn
   aws_region                       = var.aws_region
--- vars.tf
+++ vars.tf
@@ -4,104 +4,86 @@ variable "aws_region" {
 }
 
 variable "vpc_cidr_block" {
-  default = {
-    home-lab = "10.0.0.0/16"
-  }
+  default = "10.0.0.0/16"
 }
 
 variable "subnets" {
   default = {
-    home-lab = {
-      public-subnet-01 = {
-        name              = "public-subnet-01-1a"
-        cidr_block        = "10.0.1.0/24"
-        availability_zone = "us-east-1a"
-      }
+    public-subnet-01 = {
+      name              = "public-subnet-01-1a"
+      cidr_block        = "10.0.1.0/24"
+      availability_zone = "us-east-1a"
+    }
 
-      public-subnet-02 = {
-        name              = "public-subnet-02-1b"
-        cidr_block        = "10.0.2.0/24"
-        availability_zone = "us-east-1b"
-      }
+    public-subnet-02 = {
+      name              = "public-subnet-02-1b"
+      cidr_block        = "10.0.2.0/24"
+      availability_zone = "us-east-1b"
     }
   }
 }
 
 variable "gateway_name" {
-  default = {
-    home-lab = "internet-gateway"
-  }
+  default = "internet-gateway"
 }
 
 variable "routes" {
   default = {
-    home-lab = {
-      public-subnet-01 = {
-        name       = "public-subnet-route-01"
-        cidr_block = "0.0.0.0/0"
-      }
+    public-subnet-01 = {
+      name       = "public-subnet-route-01"
+      cidr_block = "0.0.0.0/0"
+    }
 
-      public-subnet-02 = {
-        name       = "public-subnet-route-02"
-        cidr_block = "0.0.0.0/0"
-      }
+    public-subnet-02 = {
+      name       = "public-subnet-route-02"
+      cidr_block = "0.0.0.0/0"
     }
   }
-
 }
 
 variable "eks_cluster_role_name" {
-  default = {
-    home-lab = "home-lab-eks-cluster-role"
-  }
+  default = "home-lab-eks-cluster-role"
 }
 
 
 variable "eks_node_group_role_name" {
-  default = {
-    home-lab = "home-lab-node-group-role"
-  }
+  default = "home-lab-node-group-role"
 }
 
 variable "eks_cluster_name" {
-  default = {
-    home-lab = "home-lab-cluster-01"
-  }
+  default = "home-lab-cluster-01"
 }
 
 variable "eks_addons" {
   default = {
-    home-lab = {
-      vpc-cni = {
-        version = "v1.18.1-eksbuild.3"
-
-      }
-
-      kube-proxy = {
-        version = "v1.30.0-eksbuild.3"
-      }
-
-      coredns = {
-        version = "v1.11.1-eksbuild.8"
-      }
+    vpc-cni = {
+      version = "v1.18.1-eksbuild.3"
 
     }
+
+    kube-proxy = {
+      version = "v1.30.0-eksbuild.3"
+    }
+
+    coredns = {
+      version = "v1.11.1-eksbuild.8"
+    }
+
   }
 }
 
 variable "eks_node_group" {
   default = {
-    home-lab = {
-      home-lab-node-group = {
-        name           = "home-lab-node-group"
-        instance_types = ["t2.micro"]
-        desired_size   = 1
-        max_size       = 2
-        min_size       = 1
-      }
+    home-lab-node-group = {
+      name           = "home-lab-node-group"
+      instance_types = ["t2.micro"]
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
     }
   }
 }
+
 
 variable "atlantis" {
   default = {
